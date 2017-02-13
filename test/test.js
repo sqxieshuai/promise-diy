682880de773fb42d42a49ef3f97348e24801c91e
xrush <sqxieshuai@gmail.com>
2017-02-13 23:19:56 +0800

    实现 1. 构造函数 2. 基础的 then 方法

--- test/test.js
+++ test/test.js
@@ -1,6 +1,21 @@
 
 var Promise = require("../src/promise");
 
-var promise = new Promise();
-
-console.log("promise instance ", promise, promise.constructor);
+new Promise(function (resolve, reject) {
+  console.log("args ", arguments);
+  setTimeout(function () {
+    console.log("call setTimeout function.");
+    // resolve("resolve from promise.");
+    reject(new Error("no value can resolve."))
+  }, 1000);
+}).then(
+  function resolveCb(data) {
+    console.log("log data from then resolve cb: ", data);
+  },
+  function rejectCb(reason) {
+    if (typeof reason == "object") {
+      reason = reason.message;
+    }
+    console.log("log reason from then reject cb: ", reason);
+  }
+);
